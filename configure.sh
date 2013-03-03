10eef48566307926cd3eec214e6c792af585bf5e
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-03-03 17:08:31 -0500

    Change the root environment from CAPTAINROOT to CAPT_ROOT.

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
-#!/bin/dash
+#! /bin/sh
 #
+# Create the login scripts (captain.profile and captain.cshrc).  This
+# rewrites input templates with installation specific values.
 #
+
 
 usage () {
     cat <<EOF
@@ -30,27 +33,56 @@ if [ x.git != x$(git rev-parse --git-dir) ]; then
     exit 1; 
 fi
 
-CAPTAINROOT=$(dirname ${PWD})
+# Handle the input parameters.  This is mostly copied from the getopt
+# documentation.  It relies on gnu getopt.
 
-cat inputs/captain.profile.in | \
-    sed "s:@@CAPTAINROOT@@:${CAPTAINROOT}:g" \
-    > captain.profile
+TEMP=$(getopt -o ho:u: \
+    --long curl,help,output:,url-root:,wget \
+    -- "$@")
 
-cat inputs/captain.cshrc.in | \
-    sed "s:@@CAPTAINROOT@@:${CAPTAINROOT}:g" \
-    > captain.cshrc
+if [ $? != 0 ]; then
+    echo
+    echo "Try '$0 --help'"
+    exit 1;
+fi
+
+eval set -- "${TEMP}"
+
+while true; do
+    case "$1" in
+	-h|--help) usage; shift; exit 0;;
+	--) break;;
+	*) break;;
+    esac
+done
+shift
+
+#####################################################################
+# Find the base directory for this installation, and set it in the
+# login scripts.  The login scripts have this parameterized as
+# "@@CAPTAINROOT@@".
+
+CAPT_ROOT=$(dirname ${PWD})
+for input in inputs/*.in; do
+    output=`basename ${input} .in`
+    echo "Write $output from $input"
+    cat $input | \
+    sed "s:@@CAPTAINROOT@@:${CAPT_ROOT}:g" \
+    > $output
+done
 
 cat <<EOF
+
 Setup files named 
 
-   ${CAPTAINROOT}/CAPTAIN/captain.profile
-   ${CAPTAINROOT}/CAPTAIN/captain.cshrc
+   ${CAPT_ROOT}/CAPTAIN/captain.profile
+   ${CAPT_ROOT}/CAPTAIN/captain.cshrc
 
-have been created.  Users can source these files directly, or add the
-following aliases to their login scripts to simplify CAPTAIN setup.
+have been created.  You can source these files directly, or add the
+following aliases to the login scripts to simplify CAPTAIN setup.
 
-sh:  alias capt-setup=". ${CAPTAINROOT}/CAPTAIN/captain.profile"
-csh: alias capt-setup "source ${CAPTAINROOT}/CAPTAIN/captain.cshrc"
+sh:  alias capt-setup=". ${CAPT_ROOT}/CAPTAIN/captain.profile"
+csh: alias capt-setup "source ${CAPT_ROOT}/CAPTAIN/captain.cshrc"
 
 The environment will then be setup by running "capt-setup" If this
 command is run from inside of a CAPTAIN package or project, the local
