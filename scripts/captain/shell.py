7dda8ad662c9b2a1ff04a456e4dbd18c070dcbda
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-03-22 11:50:48 -0400

    Add quiet flag to captain.shell module methods.
    All of the methods in shell now have an optional quiet flag.  The default
    is for the commands to be quiet.

--- scripts/captain/shell.py
+++ scripts/captain/shell.py
@@ -10,28 +10,25 @@
 
 import subprocess
 
-def CaptureShell(command):
+def CaptureShell(command,quiet=True):
     """ Return a tuple with the first element being the stdout, 
     the second being stderr, and the third being the status"""
-
+    if not quiet: print "$", command
     result = subprocess.Popen(command,
                               shell=True,
                               universal_newlines=True,
                               stdout=subprocess.PIPE, 
                               stderr=subprocess.PIPE)
-
     return (result.stdout.read(), result.stderr.read(), result.returncode)
 
-def Shell(command,quiet=False):
-    """ Return a tuple with the first element being the stdout, 
-    the second being stderr, and the third being the status"""
-
+def Shell(command,quiet=True):
+    """ Run a command in a shell.  This has no return value, but will
+    raise a subprocess.CallProcessError if the shell return code is
+    not zero."""
     if not quiet: print "$", command
-
     result = subprocess.Popen(command,
                               shell=True,
                               universal_newlines=True)
     retcode = result.wait();
-
     if retcode != 0:
         raise subprocess.CalledProcessError(retcode,command)
